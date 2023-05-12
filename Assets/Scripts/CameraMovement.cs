baf02c6838f5f9ae0aa9aeee3d2bdb137ecb3842
LucasNijland <103414594+LucasNijland@users.noreply.github.com>
2023-05-12 09:44:22 +0200

    update

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 public class CameraMovement : MonoBehaviour
 {
 
-    private Vector3 offset = new Vector3 (0, 0, -10f);
-    [SerializeField] private float smoothTime= 0.25f;
+    private Vector3 offset = new Vector3(0, 0, -10f);
+    [SerializeField] private float smoothTime = 0.25f;
     private Vector3 velocity = Vector3.zero;
 
     [SerializeField] private Transform player;
@@ -22,6 +22,5 @@ public class CameraMovement : MonoBehaviour
         // update position
         Vector3 targetPosition = player.position + offset;
         transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
-
     }
 }
--- Assets/Scripts/ScreenShake.cs
+++ Assets/Scripts/ScreenShake.cs
@@ -1,34 +1,57 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.Rendering.PostProcessing;
 public class ScreenShake : MonoBehaviour
 {
-    [SerializeField] private GameObject camera;
-    [SerializeField] private bool hasEffect;
-    [SerializeField] private float duration = 3f;
-    [SerializeField] private float tempDuration;
+    [SerializeField] private Transform camera;
+
+    // Desired duration of the shake effect
+    [SerializeField] private float shakeDuration = 0f;
+
+    // A measure of magnitude for the shake. Tweak based on your preference
+    [SerializeField] private float shakeMagnitude = 0.7f;
+
+    // A measure of how quickly the shake effect should evaporate
+    [SerializeField] private float dampingSpeed = 1.0f;
+
+    // The initial position of the GameObject
+    Vector3 initialPosition;
+
+    void Awake()
+    {
+        if (transform == null)
+        {
+            camera = GetComponent(typeof(Transform)) as Transform;
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        tempDuration = duration;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (hasEffect)
+        if (shakeDuration > 0)
         {
-            if (tempDuration > 0)
-            {
-                tempDuration -= Time.deltaTime;
-                camera.transform.localRotation = Quaternion.Euler(0, 0, Random.Range(-22.5f, 22.5f));
-            }
-            if (tempDuration <= 0)
-            {
-                hasEffect = false;
-                tempDuration = duration;
-            }
+            transform.localPosition = initialPosition + Random.insideUnitSphere * shakeMagnitude;
+
+            shakeDuration -= Time.deltaTime * dampingSpeed;
         }
+        else
+        {
+            shakeDuration = 0f;
+            transform.localPosition = initialPosition;
+        }
+    }
+    void OnEnable()
+    {
+        initialPosition = transform.localPosition;
+    }
+    public void TriggerShake()
+    {
+        shakeDuration = 2.0f;
     }
 }
