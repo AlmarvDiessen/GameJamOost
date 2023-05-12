957c8f41d1337cabfe426f136a9cff1de7db4895
AlmarvDiessen <0330841@student.rocvantwente.nl>
2023-05-12 12:50:30 +0200

    ScreenShake interval

--- Assets/Scripts/ScreenShake.cs
+++ Assets/Scripts/ScreenShake.cs
@@ -2,49 +2,46 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Rendering.PostProcessing;
-public class ScreenShake : MonoBehaviour
-{
-    [SerializeField] private Transform camera;
+public class ScreenShake : MonoBehaviour {
+
 
     // Desired duration of the shake effect
-    [SerializeField] private float shakeDuration = 0f;
+    [SerializeField] private float shakeDuration = 1f;
 
     // A measure of magnitude for the shake. Tweak based on your preference
-    [SerializeField] private float shakeMagnitude = 0.7f;
+    [SerializeField] private float shakeMagnitude = 0.1f;
 
     // A measure of how quickly the shake effect should evaporate
     [SerializeField] private float dampingSpeed = 1.0f;
+    public float shakeInterval;
 
 
-    void Awake()
-    {
-        if (transform == null)
-        {
-            camera = GetComponent(typeof(Transform)) as Transform;
-        }
+    void Awake() {
+
     }
 
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
+        shakeInterval = 4f;
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        if (shakeDuration > 0)
-        {
-            transform.localPosition = transform.localPosition + Random.insideUnitSphere * shakeMagnitude;
-
+    void Update() {
+        do {
+            if (shakeDuration >= 0) {
+                transform.localPosition = transform.localPosition + Random.insideUnitSphere * shakeMagnitude;
+            }
             shakeDuration -= Time.deltaTime * dampingSpeed;
-        }
-        else
-        {
-            shakeDuration = 0f;
+            shakeInterval -= Time.deltaTime * dampingSpeed;
+
+        } while (shakeDuration >= 0 && shakeInterval <= 0);
+
+        if (shakeInterval <= 0 && shakeDuration < 0) {
+            shakeDuration = 0.02f;
+            shakeInterval = 4f;
         }
     }
-    public void TriggerShake()
-    {
-        shakeDuration = 2.0f;
+    public void TriggerShake() {
+        shakeDuration = 1.0f;
     }
 }
